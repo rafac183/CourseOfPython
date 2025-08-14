a4ef0f657efced5c45094b4037b9eab4205f522e
unknown <rafaelantoniocordero2002mail.com>
2025-08-14 19:03:21 -0400

    Tuplas

---
+++ Python/Python.md
@@ -0,0 +1,15 @@
+Tuplas
+Para crear una tupla, encierra los elementos entre paréntesis:
+
+punto = (3, 4)
+
+Para acceder a los elementos de una tupla, utiliza el índice del elemento entre corchetes, similar a las listas:
+
+```
+print(punto[0])  # Imprime 3  
+  
+print(punto[1])  # Imprime 4
+```
+A diferencia de las listas, las tuplas son inmutables, lo que significa que no se pueden modificar una vez creadas. No se pueden agregar, eliminar o cambiar elementos en una tupla existente.
+
+Las tuplas son útiles cuando necesitas almacenar una colección de elementos que no deben modificarse, como coordenadas o datos de configuración.
--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-frutas = ["manzana", "banana", "naranja"]
+punto = (3, 4)
 
-""" print(frutas[0])
-print(frutas[2])  
-print("\n")
-print(frutas[-1])
-print(frutas[-2])
-print(frutas[-3]) """
+tuplas = (1,2,3,2,4,2)
 
-""" #Agregar a lista
-frutas.append("pera")
-print(frutas)
-
-#Agregar a lista en lugar especifico
-frutas.insert(1, "uva")
-print(frutas)
-
-#Eliminar de lista
-frutas.remove("banana")
-print(frutas)
-
-#Eliminar de lista en lugar especifico
-fruta_eliminada = frutas.pop(2)
-print(frutas)
-print(fruta_eliminada)
-
-frutas.sort()
-print(frutas)
-
-frutas.reverse()
-print(frutas) """
-
-numeros = [1,2,3,4,5]
-cuadrados = [x**2 for x in numeros if x % 3 == 0]
-print(cuadrados)
+print (tuplas.count(2))
+print (len(tuplas))
+print (tuplas.index(2))
+print (tuplas.index(2, 4))
+print (tuplas.index(2, 2, 4))
