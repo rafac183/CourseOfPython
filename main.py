d0c0c59418ca2c55045b58a603e295f9111c5e5d
unknown <rafaelantoniocordero2002mail.com>
2025-08-14 19:24:06 -0400

    Conjuntos

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-persona = {"nombre": "Juan", "edad": 25, "ciudad": "Madrid"}
+frutas = {"manzana", "banana", "naranja"}
+"""numeros = set([1, 2, 3, 4, 5])
 
-print(persona["nombre"])
-print(persona["edad"])  
-print(persona["ciudad"])
+conjunto1 = {1, 2, 3}
+conjunto2 = {3, 4, 5}
 
-print(persona.keys())
-print(persona.values())
-print(persona.items())
 
-persona.update({"profesion": "Ingeniero"})
-print(persona)
+union = conjunto1 | conjunto2
+print(union)  # Imprime {1, 2, 3, 4, 5}
+
+
+interseccion = conjunto1 & conjunto2
+print(interseccion)  # Imprime {3}
+
+
+diferencia = conjunto1 - conjunto2
+print(diferencia)  # Imprime {1, 2}
+
+
+diferencia_simetrica = conjunto1 ^ conjunto2
+print(diferencia_simetrica)  # Imprime {1, 2, 4, 5} """
+
+frutas.add("pera")
+print(frutas)  # Imprime {"manzana", "banana", "naranja", "pera"}
+
+
+frutas.remove("banana")
+print(frutas)  # Imprime {"manzana", "naranja", "pera"}
+
+
+frutas.discard("uva")#si no esta no hace nada
+print(frutas)  # Imprime {"manzana", "naranja", "pera"}
+
+
+frutas.clear()
+print(frutas)  # Imprime set()
